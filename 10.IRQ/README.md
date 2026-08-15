909afb5b01aa0d38f7730e5f53c5a90adc45e288
Rjomik <roman.rychkov@outlook.com>
2026-08-15 20:50:10 +0300

    workqueues

--- 10.IRQ/README.md
+++ 10.IRQ/README.md
@@ -1,3 +1,26 @@
 cat /proc/interrupts
 mod_timer - modify timer, if timer already running it is restarted.
     (when (*expired > clicking frequency) the callback never fires)
+
+schedule_work(struct work_struct*) - protects from double pending, but can result into running&pedning
+    if pending bit == 0 
+        then pending = 1
+    else 
+        ignore
+
+Output:
+[ 3333.551452] Button pressed!
+[ 3333.551544] Starting a work - pedning bit 0, running bit 1
+[ 3333.730646] Button pressed! - sets pending bit to 1
+[ 3333.895841] Button pressed! - ingored
+[ 3334.047796] Button pressed! - ingored
+[ 3334.195218] Button pressed! - ingored
+[ 3334.336622] Button pressed! - ingored
+[ 3334.590270] Button pressed! - ingored
+[ 3334.731764] Button pressed! - ingored
+[ 3334.914779] Button pressed! - ingored
+[ 3335.181388] Button pressed! - ingored
+[ 3335.333254] Button pressed! - ingored
+[ 3336.671148] Switching a led - sleep end, quits the work, checks the pending bit
+[ 3336.671187] Starting a work - Oh sh*t I am pedning, run again
+[ 3339.743113] Switching a led - another sleep end
---
+++ 10.IRQ/bottomHalf.c
@@ -0,0 +1,97 @@
+#include <linux/module.h> 
+#include <linux/kern_levels.h> //KERN_ERR, KERN_INFO 
+#include <linux/kernel.h> //printk
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/workqueue.h>
+#include <linux/delay.h>
+
+#define PROCFS_NAME "GPIO Button interrupt handling with workqueues" 
+#define SIZE 1024
+
+#define GPIO_OFFSET 0
+#define ALLWINNER_PC_OFFSET 64
+#define LED_GPIO 9
+#define BUTTON_GPIO 11
+
+struct work_struct buttonWork;
+
+static int led_gpio = (GPIO_OFFSET + ALLWINNER_PC_OFFSET + LED_GPIO);
+static int button_gpio = (GPIO_OFFSET + ALLWINNER_PC_OFFSET + BUTTON_GPIO);
+
+static int irqNum = 0;
+#define WORKQUEUE_DELAY 3000
+static irqreturn_t buttonHandler(int irq, void *dev)
+{
+    pr_info("Button pressed!\n");
+    schedule_work(&buttonWork);
+    return IRQ_HANDLED;
+}
+
+static void WorkQueueFunction(struct work_struct *work){
+    
+    pr_info("Starting a work\n");
+    msleep(WORKQUEUE_DELAY);
+    pr_info("Switching a led\n");
+    gpio_set_value(led_gpio, !gpio_get_value(led_gpio));
+}
+
+static int __init helloInit(void)
+{
+ 
+    int status = gpio_request(led_gpio, "led_gpio");
+    if(status)
+    {
+        pr_err("Failed to request led gpio");
+        return -status;
+    }
+
+    gpio_direction_output(led_gpio, 0);
+
+    status = gpio_request(button_gpio, "button_gpio");
+    if(status)
+    {
+        pr_err("Failed to request button gpio");
+        return -status;
+    }
+
+    gpio_direction_input(button_gpio);
+
+    irqNum = gpio_to_irq(button_gpio);
+    if(irqNum < 0)
+    {
+        pr_err("Failed to get the IRQ num from GPIO num");
+        return -status;
+    }
+
+    status = request_irq(irqNum, buttonHandler, IRQF_TRIGGER_FALLING, "My little button", NULL);
+    if(status)
+    {
+        pr_err("Failed to request an interrupt");
+        return -status;
+    }
+
+
+    pr_alert("The interrupt number is: %d\n", irqNum);
+
+    INIT_WORK(&buttonWork, WorkQueueFunction);
+
+    printk(KERN_INFO "Successfully loaded module %s\n", PROCFS_NAME);
+    return 0; 
+}
+
+static void __exit helloExit(void)
+{
+    gpio_set_value(led_gpio, 0);
+    gpio_free(led_gpio);
+    gpio_free(button_gpio);
+    free_irq(irqNum, NULL);
+    cancel_work_sync(&buttonWork);
+    printk(KERN_INFO "Successfully unloaded module %s\n", PROCFS_NAME);
+}
+module_init(helloInit);
+module_exit(helloExit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rjomik");
+MODULE_DESCRIPTION("First module on orange pi");
